ea42a24b854ca7b3075302a792ef6265d2ba6854
matheuslr <matheuslr27@gmail.com>
2022-04-29 16:36:30 -0300

    test: :white_check_mark: add health and get_app routes

---
+++ tests/unit/healthcheck/test_views.py
@@ -0,0 +1,22 @@
+
+from tests.conftest import TestClient
+from time import time
+from freezegun import freeze_time
+
+def test_should_get_app(client:TestClient):
+    response = client.get("/")
+    result = response.json()
+
+    assert response.status_code == 200
+    assert result == [{"app": "sbf-challenge"}]
+
+@freeze_time("2022-01-14")
+def test_should_get_health(client:TestClient):
+    response = client.get("/health")
+    result = response.json()
+
+    assert response.status_code == 200
+    assert result == {
+    "healthy": True,
+    "checked_at": time()
+    }
